94608cb11d0da097955c0c5fdcd2101a5d9597a3
Linus Juhlin <juhlinus@gmail.com>
2018-04-29 15:03:21 +0900

    Clean Project

---
+++ Game1.cs
@@ -0,0 +1,14 @@
+﻿using Nez;
+
+namespace FlappyFlop
+{
+    public class Game1 : Core
+    {
+        public Game1() { }
+
+        protected override void Initialize()
+        {
+            base.Initialize();
+        }
+    }
+}
